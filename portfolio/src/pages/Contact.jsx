0bdfb3cfbc6e0e59eee69d52994df2ba450fcb86
Michael Canales <CDLSMichael.16@gmail.com>
2023-01-21 16:05:10 -0500

    problema resuelto

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -13,6 +13,7 @@ const Contact = () => {
   const [form, setForm] = useState(initialState);
   const [errors, setErrors] = useState({
     msg: "",
+    successMsg: "",
   });
 
   const handleSubmit = (e) => {
@@ -23,9 +24,10 @@ const Contact = () => {
 
     if (noEmty.includes("")) {
       setErrors({
+        ...errors,
         msg: "ningun Campo debe quedar vacio",
       });
-      setTimeout(() => setErrors({ msg: "" }), 5000);
+      setTimeout(() => setErrors({ ...errors, msg: "" }), 5000);
       return;
     }
 
@@ -37,10 +39,35 @@ const Contact = () => {
     ];
     emailjs
       .sendForm(...credentials)
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        if (error) {
+          setErrors({
+            ...errors,
+            msg: "Lo sentimos ocurrió un error inesperado, intentalo mas tarde",
+          });
+          setTimeout(() => {
+            setErrors({
+              ...errors,
+              msg: "",
+            });
+          }, 5000);
+        }
+      });
+
+    setErrors({
+      ...errors,
+      successMsg: "email enviado con éxito :)",
+    });
+
+    setTimeout(() => {
+      setErrors({ ...errors, successMsg: "" });
+    }, 5000);
 
     setForm(initialState);
+    setLimitText(250);
   };
 
   const handleChange = (e) => {
@@ -121,6 +148,11 @@ const Contact = () => {
               </div>
               <div className={`${errors.msg ? "block mt-3" : "hidden"}`}>
                 <p className="text-center text-red-600 text-sm">{errors.msg}</p>
+              </div>
+              <div className={`${errors.successMsg ? "block mt-3" : "hidden"}`}>
+                <p className="text-center text-green-600 text-sm">
+                  {errors.successMsg}
+                </p>
               </div>
             </div>
           </form>
